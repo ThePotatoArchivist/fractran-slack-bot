d2e2399f2c48b59ece1e75d9bbb5e333a5d3a2d8
JosiahFu <josiahfu@gmail.com>
2025-08-03 17:21:34 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,110 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+import fractran
+import traceback
+from collections import defaultdict
+from textwrap import dedent
+
+# This sample slack application uses SocketMode
+# For the companion getting started setup guide, 
+# see: https://slack.dev/bolt-python/tutorial/getting-started 
+
+# Initializes your app with your bot token
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+def parse_factor(frac: str):
+    split = frac.split('^')
+    return int(split[0]), 1 if len(split) < 2 else int(split[1])
+
+def parse_factors(factors: str):
+    return defaultdict(int, dict(parse_factor(c) for c in factors.split('*')))
+
+def parse_fraction(fracstr: str):
+    for b in fracstr.split('/'):
+        yield parse_factors(b)
+
+def parse(strinput: str):
+    *fractions, input = strinput.split(' ')
+    
+    return [list(parse_fraction(a)) for a in fractions], parse_factors(input)
+
+def handle_error(say, e: Exception, msg: str):
+    print(traceback.format_exc())
+    response = f'{msg}:\n```{type(e).__name__}: {e}```'
+    
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": response},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Dismiss"},
+                    "action_id": "button_click",
+                },
+            }
+        ],
+        text=response,
+    )
+
+
+# Listens to incoming messages that contain "hello"
+@app.command('/fractran')
+def message_hello(ack, say, command):
+    ack()
+    
+    error = False
+
+    try:
+        fractions, input = parse(command['text'])
+      
+    except (IndexError, ValueError) as e:
+        handle_error(say, e, 'Parsing Error')
+        return
+
+    try:
+        output, steps = fractran.execute(input, fractions)
+    except Exception as e:
+        handle_error(say, e, 'Execution Error')
+        return
+    
+    response = dedent(f'''\
+        <@{command['user_id']}>
+        Input: `{command['text']}`
+        Output: ```''') + \
+        output + \
+        dedent(f'''\
+        ```
+        Finished in {steps} steps
+        Total fractions: {len(fractions)}
+        Latex: `{fractran.generate_latex(fractions)}`
+        ''')
+
+    # say() sends a message to the channel where the event was triggered
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": response},
+            }
+        ],
+        text=response,
+    )
+
+
+@app.action("button_click")
+def action_button_click(body, ack, say):
+    # Acknowledge the action
+    ack()
+    result = app.client.chat_delete(
+        channel=body['container']['channel_id'],
+        ts=body['container']['message_ts']
+    )
+    if not result['ok']:
+        print(result)
+
+
+# Start your app
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
---
+++ fractran.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 51, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+def generate_latex(fractions):
+    output = ""
+    for fraction in fractions:
+        output += "\\frac{"
+        if len(fraction[0]) == 0:
+            output += "1"
+        for base, exponent in fraction[0].items():
+            output += str(primes[base])
+            if exponent > 1:
+                output += "^{" + str(exponent) + "}"
+            output += " \\cdot "
+        if output.endswith(" \\cdot "):
+            output = output[:-7]
+        output += "}{"
+        for base, exponent in fraction[1].items():
+            output += str(primes[base])
+            if exponent > 1:
+                output += "^{" + str(exponent) + "}"
+            output += " \\cdot "
+        if output.endswith(" \\cdot "):
+            output = output[:-7]
+        output += "}, "
+    if output.endswith(", "):
+        output = output[:-2]
+    return output
+
+def non_zero_print(d):
+    return str({k: v for k, v in d.items() if v != 0}) + '\n'
+
+def do_once(i, fractions):
+    output = i.copy()
+    for fraction in fractions:
+        n = fraction[0]
+        d = fraction[1]
+        good = True
+        for type, num in d.items():
+            if i[type] < num:
+                good = False
+        if good:
+            for type, num in d.items():
+                output[type] -= num
+            for type, num in n.items():
+                output[type] += num
+            return output
+    return "end"
+
+def execute(input, fractions):
+    output = ''
+    i=0
+    while True:
+        i += 1
+        output += non_zero_print(input)
+        input = do_once(input, fractions)
+        if input == "end" or i > 100000:
+            return output, i
